24d37b4fba48a4f2540886c85fc641c452400683
C++ Lab projects <mdanamulh696@gmail.com>
2025-11-26 22:43:46 +0100

    day1

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print("hello world")
+print("My name is md anamul haque\n i am 31 yeras old\n i live at duisburg in germany\n i am studing software at the university of duisburg essen\n ")
+print("today is my 1st day, i stated learning python")
