0fe54abe59e01dd9cd7550368214f8ff0a98d6d8
C++ Lab projects <mdanamulh696@gmail.com>
2025-11-27 12:38:55 +0100

    Lecture1 done

---
+++ Lecture1/Variable_DataType.py
@@ -0,0 +1,62 @@
+# # variable
+# print("hello world")
+# info = "My name is md anamul haque\ni am 31 yeras old\ni live at duisburg in germany\ni am studing software at the university of duisburg essen"
+# print(info)
+# print("today is my 1st day, i stated learning python")
+
+# print("this is my details :", info)
+
+# # Data Type
+
+# name = "anamul"
+# age = 31
+# price = 29.50
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# # Category Keywords
+
+# import keyword
+# print(keyword.kwlist)
+
+# name = input("enter your name:")
+# print(name)
+
+# val = str(input("enter some value: "))
+# print(type(val), val)
+
+# name = input("enter name: ")
+# age = int(input("enter age: "))
+# markes = float(input(" enter markes: "))
+
+# print("welcome", name)
+# print("age =", age)
+# print("markes = ", markes)
+
+# # write a program to input 2 numbers & print their sum
+# a = int(input("enter 1st number: "))
+# b = int(input("enter 2nd number: "))
+# sum = a + b
+# print(sum)
+
+# # write a program to input of a square & print its area.
+# a = int(input("enter the square side value: "))
+# area = a*a
+# print(area)
+
+# # write a program to input 2 floating point num & print their average
+# a = float(input("enter 1st floating numver: "))
+# b = float(input("enter 2nd floating numver: "))
+# avg = (a + b)/2
+# print(avg)
+
+# # write a program to input 2 int numbers a and b 
+# # print true if a is greater than or equal to b. if not print false.
+# a = int(input("enter 1st int number: "))
+# b = int(input("enter 2nd int number: "))
+# if a>=b:
+#    print(True)
+# else :
+#  print(False)
