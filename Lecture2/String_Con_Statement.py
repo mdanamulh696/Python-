12278b4b8261ae6bb8adad75ee163d1291b80ec2
C++ Lab projects <mdanamulh696@gmail.com>
2025-11-28 13:28:42 +0100

    Lecture2

---
+++ Lecture2/String_Con_Statement.py
@@ -0,0 +1,12 @@
+str1 = "This is a string."
+str2 = 'Md Aanamul Haque.'
+str3 = """This is a string an another way."""
+
+print(str1, "it's lenght:",len(str1))
+print(str2, "it's lenght:",len(str2))
+print(str3, "it's lenght:",len(str3))
+
+final_str = str1 +" "+ str2 +" "+ str3
+
+print(final_str)
+print(len(final_str))
