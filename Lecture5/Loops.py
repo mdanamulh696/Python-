faa47175bb464f642f404768b2dd8391f824b8cf
C++ Lab projects <mdanamulh696@gmail.com>
2025-12-07 23:54:37 +0100

    Lecture 5

--- Lecture5/Loops.py
+++ Lecture5/Loops.py
@@ -1,5 +1,56 @@
-count = 1
-while count <= 5 :
-    print("hi i am in loops", count)
-    count += 1
-print(count)
+# count = 1
+# while count <= 5 :
+#     print("hi i am in loops", count)
+#     count += 1
+# print(count)
+
+# Lets Practice
+
+# # Print numbers from 1 to 100.
+# i = 1
+# while i <=100:
+#     print(i)
+#     i += 1
+
+# # Print numbers from 100 to 1.
+# i = 100
+# while i >=1:
+#     print(i)
+#     i -= 1
+
+# # Print the multiplication table of a number n.
+
+# n = int(input("enter any , which you want to multiplication "))
+# i = 1
+# while i <=10:
+#     print(n*i)
+#     i += 1
+
+# # Print the elements of the following list using a loop: 
+# # [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# # ["ironman", "thor", "superman", "batman"]    
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]  
+# heroes = ["ironman", "thor", "superman", "batman"]
+
+
+# i = 0
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+# i = 0
+# while i < len(heroes):
+#     print(heroes[i])
+#     i += 1
+
+# Search for a number x in this tuple using loop:
+# (1, 4, 9, 16, 25, 36, 49, 64, 81,100) 
+
+# x = int(input("enter any value from tuple, which you want to find "))
+# num = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+# i =0 
+# while i < len(num):
+#     if(num[i] == x):
+#         print("found at index", i)
+#     i += 1
